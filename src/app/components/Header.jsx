4f0d85601a8bca7e2d39ee4480632f8e798fff09
gsspg <gauravlandge12307@gmail.com>
2024-06-16 23:38:11 +0530

    add modal functionality using react modal

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,14 +1,19 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
-import { signIn, useSession,signOut } from "next-auth/react";
+import React, { useState } from "react";
+import { signIn, useSession, signOut } from "next-auth/react";
+import Modal from "react-modal";
+import { IoMdAddCircleOutline } from "react-icons/io";
+import { HiCamera } from "react-icons/hi";
+import { AiOutlineClose } from "react-icons/ai";
 
 export default function Header() {
   const { data: session } = useSession();
-  console.log(session);
+  const [isOpen, setIsOpen] = useState(false);
+  //console.log(session);
   return (
-    <div classname="shadow-sm border-b sticky top-0 bg-white z-30 p-3">
+    <div className="shadow-sm border-b sticky top-0 bg-white z-30 p-3">
       <div className="flex justify-between items-center max-w-6xl mx-auto">
         {/* logo */}
         <Link href="/" className="hidden lg:inline-flex">
@@ -18,7 +23,6 @@ export default function Header() {
             height={96}
             alt="insta logo"
           ></Image>
-
         </Link>
         <Link href="/" className="lg:hidden">
           <Image
@@ -36,18 +40,56 @@ export default function Header() {
           className="bg-gray-50 border border-color-gray-200 rounded text-sm w-full py-2 px-4 max-w-[210px]"
         />
         {/* menu items */}
-        {session?(
-          <img src={session.user.image}
-          className="h-10 w-10 rounded-full cursor-pointer" onClick={signOut}
-           alt={session.user.name}></img>
-        ):(<button
-          onClick={signIn}
-          className="text-sm font-semibold text-blue-500"
-        >
-          Log In
-        </button>)}
-        
+        {session ? (
+          <div className="flex gap-2 items-center">
+            <IoMdAddCircleOutline
+              className="text-2xl cursor-pointer transform hover:scale-125 transition duration-300 hover:text-red-600"
+              onClick={() => setIsOpen(true)}
+            />
+
+            <img
+              src={session.user.image}
+              className="h-10 w-10 rounded-full cursor-pointer"
+              onClick={signOut}
+              alt={session.user.name}
+            ></img>
+          </div>
+        ) : (
+          <button
+            onClick={signIn}
+            className="text-sm font-semibold text-blue-500"
+          >
+            Log In
+          </button>
+        )}
       </div>
+
+      {isOpen && (
+        <Modal
+          isOpen={isOpen}
+          className="max-w-lg w-[90%] p-6 absolute top-56 left-[50%] translate-x-[-50%] bg-white border-2 rounded-md shadow-md"
+          onRequestClose={() => setIsOpen(false)}
+          ariaHideApp={false}
+        >
+          <div className="flex flex-col justify-center items-center h-[100%">
+            <HiCamera className="text-5xl text-gray-400 cursor-pointer"></HiCamera>
+          </div>
+
+          <input
+            type="text"
+            maxLength="150"
+            placeholder="Please Enter Your Caption ...."
+            className="m-4 border-none text-center w-full focus:ring-0 outline-none"
+          ></input>
+          <button className="w-full bg-red-600 text-white p-2 shadow-md rounded-lg hover:brightness-105 disabled:bg-gray-200 disabled:cursor-not-allowed disabled:hover:brightness-100">
+            Upload Post
+          </button>
+          <AiOutlineClose
+            className="cursor-pointer absolute top-2 right-2 hover:text-red-600 transition duration-300"
+            onClick={() => setIsOpen(false)}
+          />
+        </Modal>
+      )}
     </div>
   );
 }
