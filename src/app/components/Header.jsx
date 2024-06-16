9016cc26a666792b36946af03cfd0c5c489108af
gsspg <gauravlandge12307@gmail.com>
2024-06-16 18:15:20 +0530

    add next-auth and modify the header component

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,8 +1,12 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { signIn, useSession,signOut } from "next-auth/react";
 
 export default function Header() {
+  const { data: session } = useSession();
+  console.log(session);
   return (
     <div classname="shadow-sm border-b sticky top-0 bg-white z-30 p-3">
       <div className="flex justify-between items-center max-w-6xl mx-auto">
@@ -14,8 +18,9 @@ export default function Header() {
             height={96}
             alt="insta logo"
           ></Image>
+
         </Link>
-        <Link href="/" className="hidden ">
+        <Link href="/" className="lg:hidden">
           <Image
             src="/800px-Instagram_logo_2016.webp"
             width={40}
@@ -31,8 +36,17 @@ export default function Header() {
           className="bg-gray-50 border border-color-gray-200 rounded text-sm w-full py-2 px-4 max-w-[210px]"
         />
         {/* menu items */}
-
-        <button className="text-sm font-semibold text-blue-500">Log In</button>
+        {session?(
+          <img src={session.user.image}
+          className="h-10 w-10 rounded-full cursor-pointer" onClick={signOut}
+           alt={session.user.name}></img>
+        ):(<button
+          onClick={signIn}
+          className="text-sm font-semibold text-blue-500"
+        >
+          Log In
+        </button>)}
+        
       </div>
     </div>
   );
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,9 @@
+"server side";
+
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
+import SessionWrapper from "./components/SessionWrapper";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,12 +14,14 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-      <Header></Header>
-      
-      {children}
-      </body>
-    </html>
+    <SessionWrapper>
+      <html lang="en">
+        <body className={inter.className}>
+          <Header></Header>
+
+          {children}
+        </body>
+      </html>
+    </SessionWrapper>
   );
 }
