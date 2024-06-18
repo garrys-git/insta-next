b636f203585bfffd21312f36aeaf457a0e736de4
gsspg <gauravlandge12307@gmail.com>
2024-06-18 11:10:39 +0530

    make the feed section responsive

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,21 @@
 import React from "react";
+import Feed from "./components/Feed";
+import Posts from "./components/Posts";
+import MiniProfile from "./components/MiniProfile";
 
 export default function Home() {
-  return <h1 className="text-red-700"> Home</h1>;
+  return (
+    <>
+      <main className="grid grid-cols-1 md:grid-cols-3 md:max-w-6xl   mx-auto">
+        {/* Posts.jsx */}
+        <section className="md:col-span-2">
+          <Posts></Posts>
+        </section>
+        {/* Mini Profile.jsx */}
+        <section className="hidden md:inline-grid md:col-span-1">
+          <MiniProfile></MiniProfile>
+        </section>
+      </main>
+    </>
+  );
 }
