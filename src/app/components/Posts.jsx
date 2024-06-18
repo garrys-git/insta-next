00e78d0e897cb210308a8c7d77265c68483c5fdf
gsspg <gauravlandge12307@gmail.com>
2024-06-18 14:47:49 +0530

    conplete post card of the feed section (server side on interactive)

--- src/app/components/Posts.jsx
+++ src/app/components/Posts.jsx
@@ -1,5 +1,26 @@
 import React from "react";
-
-export default function Posts() {
-  return <div>Posts</div>;
+import { app } from "@/firebase";
+import {
+  collection,
+  getDocs,
+  getFirestore,
+  orderBy,
+  query,
+} from "firebase/firestore";
+import Post from "./Post";
+export default async function Posts() {
+  const db = getFirestore(app);
+  const q = query(collection(db, "posts"), orderBy("timestamp", "desc"));
+  const querySnapshot = await getDocs(q);
+  let data = [];
+  querySnapshot.forEach((doc) => {
+    data.push({ id: doc.id, ...doc.data() });
+  });
+  return (
+    <div>
+      {data.map((post) => (
+        <Post key={post.id} post={post}></Post>
+      ))}
+    </div>
+  );
 }
